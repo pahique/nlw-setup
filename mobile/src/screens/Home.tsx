f9ecf62fbc66fbc9fbe50749f9904268a654a8c2
Paulo Oliveros <paulo.hco@gmail.com>
2023-01-21 16:12:10 -0300

    Mobile - Habit screen

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { Text, View, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
@@ -10,6 +11,8 @@ const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesYTD.length;
 
 export function Home() {
+    const { navigate } = useNavigation();
+
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <Header />
@@ -29,12 +32,14 @@ export function Home() {
                         contentContainerStyle={{ paddingBottom: 100 }}>
             <View className="flex-row flex-wrap">
                 {datesYTD.map(date => (
-                    <HabitDay key={date.toISOString()} />
+                    <HabitDay key={date.toISOString()} 
+                        onPress={() => navigate('habit', { date: date.toISOString() })} />
                 ))}
 
                 {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, index) => (
-                    <View className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
-                    style={{ width: DAY_SIZE, height: DAY_SIZE }}/>
+                    <View key={index} 
+                        className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                        style={{ width: DAY_SIZE, height: DAY_SIZE }}/>
                 ))}
             </View>
             </ScrollView>
