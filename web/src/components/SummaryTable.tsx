fe948e452e0c8c6feb6a8f3ed20aef2fec33d980
Paulo Oliveros <paulo.hco@gmail.com>
2023-01-21 17:53:10 -0300

    Adding axios calls

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,6 @@
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
 import { generateDatesYTD } from "../utils/generate-dates-ytd"
 import { HabitDay } from "./HabitDay"
 
@@ -17,7 +20,29 @@ const minimumSummaryDaysSize = 18 * 7;  // 18 weeks
 
 const amountOfDaysToFill = minimumSummaryDaysSize - summaryDates.length;
 
+// type Summary = Array<{
+//     id: string;
+//     date: string;
+//     amount: number;
+//     completed: number;
+// }>
+
+type Summary = {
+    id: string;
+    date: string;
+    amount: number;
+    completed: number;
+}[]
+
 export function SummaryTable() {
+    const [summary, setSummary] = useState<Summary>([]);
+
+    useEffect(() => {
+        api.get('/summary').then(response => {
+            setSummary(response.data);
+        });
+    }, [])
+
     return (
         <div className="w-full flex">
             <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -34,10 +59,14 @@ export function SummaryTable() {
             </div>
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summaryDates.map(date => {
+                    const dayInSummary = summary.find(day => {
+                        return dayjs(date).isSame(day.date, 'day')
+                    })
                     return <HabitDay 
                                 key={date.toString()}
-                                amount={5} 
-                                completed={Math.round(Math.random() * 5)} />
+                                date={date}
+                                amount={dayInSummary?.amount} 
+                                completed={dayInSummary?.completed} />
                 })}
                 {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
                     return (
